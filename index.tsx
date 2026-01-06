f9163bf8aae61aa3ab2b5c6299683b8a320256a0
Thiago Pereira <54336514+TPsilva3251@users.noreply.github.com>
2026-01-06 09:48:22 -0300

    6 teste para git

--- index.tsx
+++ index.tsx
@@ -1,16 +1,1 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Arquivo migrado para index.html para evitar erros de MIME type no GitHub Pages
